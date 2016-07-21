b5db397e877192a84416b6ee924442b5bdefa6b3
Matt <mattzera@gmail.com>
2016-07-20 18:02:02 -0700

    Added a default request to show all pokemon stored in the TownMap database under the collection Pokemon

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var mongodb = require('mongodb');
+assert = require('assert');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
+//Create the App
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var MongoClient = require('mongodb').MongoClient;
+var db;
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Initialize connection to MongoDB once
+MongoClient.connect("mongodb://localhost:27017/TownMap", function(err, database) {
+  if(err) throw err;
+  db = database;
+});
 
-app.use('/', routes);
-app.use('/users', users);
+//Default Request to show all pokemon
+app.use('/', function(req, res, next) {
+
+  var col = db.collection('Pokemon');
+
+    // Get first two documents that match the query
+    col.find().toArray(function (err, docs) {
+      assert.equal(null, err);
+      res.json(docs)
+    });
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,7 +42,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.json({
       message: err.message,
       error: err
     });
@@ -50,7 +53,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.json({
     message: err.message,
     error: {}
   });
